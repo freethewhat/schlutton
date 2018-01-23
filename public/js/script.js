39fa524bb449c631693f8b072b2e0873613768bd
Matt Sutton <mcsutton@outlook.com>
2018-01-22 18:00:45 -0700

    added touch events

--- public/js/script.js
+++ public/js/script.js
@@ -5,6 +5,13 @@ $(document).ready(function() {
     $(".mouse").css({"top": mouseY - 200, "left": mouseX - 200})
   })
 
+  document.addEventListener('touchmove', function(event) {
+    console.log(event.touches[0].screenX);
+    var mouseX = event.touches[0].screenX;
+    var mouseY = event.touches[0].screenY;
+    $(".mouse").css({"top": mouseY - 200, "left": mouseX - 200})
+  });
+
   particlesJS.load('particles-js', '../assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
