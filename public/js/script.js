42ba4a3a769cb3be29bf5b40b6de187e2956d3d3
Matt Sutton <mcsutton@outlook.com>
2018-01-25 17:33:16 -0700

    worked on file uploads and title

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,35 @@
 $(document).ready(function() {
+  currentImage = '';
+
+  function setMousePicture() {
+    $.ajax({
+      url: "/images",
+    }).done(function(images) {
+      image = images
+      num = Math.floor(Math.random() * images.length)
+
+      while(currentImage === image[num]) {
+        num = Math.floor(Math.random() * images.length)
+      }
+
+      currentImage = images[num];
+      $('.mouse').css({"background-image" : `url('../img/${images[num]}')`})
+    });
+  }
+
+  setMousePicture();
+
+  $('#particles-js').on('click', function() {
+    setMousePicture();
+  });
+
   $('.mouse').css({"top": window.innerHeight / 2 - 200, "left": window.innerWidth / 2 -200});
 
   $(document).mousemove(function(event)  {
     var mouseX = event.originalEvent.pageX;
     var mouseY = event.originalEvent.pageY;
     $(".mouse").css({"top": mouseY - 200, "left": mouseX - 200})
-  })
+  });
 
   document.addEventListener('touchmove', function(event) {
     var mouseX = event.touches[0].screenX;
@@ -14,6 +38,18 @@ $(document).ready(function() {
   });
 
   particlesJS.load('particles-js', '../assets/particlesjs-config.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+    console.log('callback - particles.js config loaded');
+  });
+
+  $(".upload-more").on('click', function() {
+    var elem = $('.upload-form');
+    console.log(elem.hasClass('collapsed'));
+    if(!elem.hasClass('collapsed')) {
+      $('.button-text').text('More');
+      elem.addClass('collapsed');
+    } else {
+      $('.button-text').text('Less');
+      elem.removeClass('collapsed');
+    }
+  })
 })
--- server.js
+++ server.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var app = express();
+var fileUpload = require('express-fileupload');
+var path = require('path');
+var fs = require('fs');
+var randomString = require('randomstring');
 
 var port = 3001;
+var publicPath = path.join(__dirname + "/public/");
 
 app.use(express.static('public'));
+
+app.use(fileUpload());
+
+app.get('/images', function(req, res) {
+  contents = fs.readdirSync('public/img');
+  res.json(contents)
+})
+
+app.post('/upload', function(req, res) {
+  if(!req.files.sampleFile) {
+    console.log('issues')
+    return res.sendFile(path.resolve(__dirname + '/public/index.html'));
+  }
+
+  var file = req.files.sampleFile;
+  var extension = file.mimetype.split('/');
+  var random = randomString.generate(7);
+
+  fs.writeFile(
+    './public/img/' + random + '.' + extension[1],
+    req.files.sampleFile.data,
+    function(err) {
+      if(err){
+        console.log(err);
+      }
+  })
+
+  res.sendFile(path.resolve(__dirname + '/public/index.html'));
+})
 
 app.listen(port, function() {
   console.log("Server running on http://localhost:" + port)
