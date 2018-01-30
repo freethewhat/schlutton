75a2976728605a42e777f48c03ac1f5ee49b22a9
Matt Sutton <mcsutton@outlook.com>
2018-01-30 14:50:27 +0000

    updated site to disable div move

--- public/js/script.js
+++ public/js/script.js
@@ -25,18 +25,18 @@ $(document).ready(function() {
 
   $('.mouse').css({"top": window.innerHeight / 2 - 200, "left": window.innerWidth / 2 -200});
 
-  $(document).mousemove(function(event)  {
+  /* $(document).mousemove(function(event)  {
     var mouseX = event.originalEvent.pageX;
     var mouseY = event.originalEvent.pageY;
     $(".mouse").css({"top": mouseY - 200, "left": mouseX - 200})
-  });
+  }); 
 
   document.addEventListener('touchmove', function(event) {
     var mouseX = event.touches[0].screenX;
     var mouseY = event.touches[0].screenY;
     $(".mouse").css({"top": mouseY - 200, "left": mouseX - 200})
   });
-
+	*/
   particlesJS.load('particles-js', '../assets/particlesjs-config.json', function() {
     console.log('callback - particles.js config loaded');
   });
